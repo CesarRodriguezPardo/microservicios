aeeab6dce08ee86c2e5b86235cde478b8378e94d
César Rodríguez Pardo <cesar.rodriguez.pa@usach.cl>
2024-12-11 17:02:57 -0300

    Cambios

---
+++ ms-register/src/main/java/com/example/demo/entities/UserEntity.java
@@ -0,0 +1,33 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "UserEntity")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String middleName;
+    private String firstSurname;
+    private String secondSurname;
+    private String rut;
+    private String email;
+    private String password;
+    private String phone;
+    private int age;
+
+    private Boolean verified;
+}
+
---
+++ ms-register/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,11 @@
+package com.example.demo.services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/user")
+@CrossOrigin
+public class UserService {
+}
